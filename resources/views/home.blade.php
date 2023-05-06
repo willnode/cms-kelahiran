aac373a7430d1763650dd5f7cf9c5e3dcd65fbe8
Wildan M <willnode@wellosoft.net>
2023-05-06 09:20:25 +0700

    Improve homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card text-center">
+                <div class="card-header">{{ __('Selamat Datang') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <h1>{{ __('Selamat Datang di Aplikasi Sistem Informasi Kelahiran Penduduk') }}</h1>
+                    
+                    <img alt="" height="300px" class="my-4" src="{{ env('APP_LOGO', 'https://picsum.photos/id/26/367/267') }}" />
+                    <h3 class="my-4">Kecamatan {{ env('APP_KECAMATAN') }} Kabupaten {{ env('APP_KABUPATEN') }} </h3>
+                    @guest
+                    <p>{{ __('Silahkan Login untuk melanjutkan') }}</p>
+                    @endguest
 
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => env('APP_DEBUG', false), // Registration Routes...
+    'reset' =>  env('APP_DEBUG', false), // Password Reset Routes...
+    'verify' =>  env('APP_DEBUG', false), // Email Verification Routes...
+]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::resource('desa', DesaController::class);
